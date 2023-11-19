3c63e0c28ef618cb3403a485674e99848f14f0a6
Amélie Simard <amelie.simard@polymtl.ca>
2023-11-19 00:52:42 -0500

    loading approved by theo

--- webSite/src/assets/js/loading.js
+++ webSite/src/assets/js/loading.js
@@ -1,20 +1,40 @@
+const logo = document.querySelector('#logo');
+const spinner = document.querySelector('.spinner');
+
 async function delay(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
 async function fadeIn() {
-    const logo = document.querySelector('#logo');
-    await delay(3500);
+    logo.style.opacity = 0;
+
+    for (let opacity = 0; opacity <= 1; opacity += 0.01) {
+        await delay(30); // Adjust the delay as needed
+        logo.style.opacity = opacity;
+    }
+
+    logo.style.opacity = 1;
 }
 
 async function showSpinner() {
-    const spinner = document.querySelector('.spinner');
     spinner.style.opacity = 1;
-    await delay(2500);
+    await delay(2800);
+    spinner.style.borderColor = '#bd66dd';
+    await delay(1300);
+}
+
+async function fadeOut() {
+
+    for (let opacity = 1; opacity >= 0; opacity -= 0.01) {
+        await delay(15); // Adjust the delay as needed
+        logo.style.opacity = opacity;
+        spinner.style.opacity = opacity;
+    }
 }
 
 document.addEventListener("DOMContentLoaded", async function () {
     await fadeIn();
     await showSpinner();
+    await fadeOut();
     window.location.href = 'mainMenu.html';
 });

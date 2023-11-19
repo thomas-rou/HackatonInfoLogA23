4142ae289d83810faf14ca71778796c3106e9afe
Amélie Simard <amelie.simard@polymtl.ca>
2023-11-18 22:38:41 -0500

    loading screen wait time

--- webSite/src/assets/js/loading.js
+++ webSite/src/assets/js/loading.js
@@ -1,5 +1,20 @@
-document.addEventListener("DOMContentLoaded", function () {
-    setTimeout(function () {
-        window.location.href = 'mainMenu.html';
-    }, 3000);
+async function delay(ms) {
+    return new Promise(resolve => setTimeout(resolve, ms));
+}
+
+async function fadeIn() {
+    const logo = document.querySelector('#logo');
+    while (logo.opacity != 1) {}
+}
+
+async function showSpinner() {
+    const spinner = document.querySelector('.spinner');
+    spinner.style.opacity = 1;
+    await delay(3000);
+}
+
+document.addEventListener("DOMContentLoaded", async function () {
+    await fadeIn();
+    await showSpinner();
+    window.location.href = 'mainMenu.html';
 });

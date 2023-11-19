c872d9b598e87227a8b6d80608aa2cc72e5fc880
Amélie Simard <amelie.simard@polymtl.ca>
2023-11-18 22:54:12 -0500

    loading page completed

--- webSite/src/assets/js/loading.js
+++ webSite/src/assets/js/loading.js
@@ -4,13 +4,13 @@ async function delay(ms) {
 
 async function fadeIn() {
     const logo = document.querySelector('#logo');
-    while (logo.opacity != 1) {}
+    await delay(3500);
 }
 
 async function showSpinner() {
     const spinner = document.querySelector('.spinner');
     spinner.style.opacity = 1;
-    await delay(3000);
+    await delay(2500);
 }
 
 document.addEventListener("DOMContentLoaded", async function () {

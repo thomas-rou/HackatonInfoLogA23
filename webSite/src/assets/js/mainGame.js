e198572f5baa05f31b6e73bb950be2ce01a954db
Math2128 <mathieu-3.lavoie@polymtl.ca>
2023-11-19 02:54:28 -0500

    Avancement mainGame. J'ai essayé de preload les images dans la cache pour éviter que lorsqu'on clique il y ait un délai, mais ça ne marche pas très bien. Les images ne semblent pas se trouver dans la cache

--- webSite/src/assets/js/mainGame.js
+++ webSite/src/assets/js/mainGame.js
@@ -1,1 +1,44 @@
-// this is the start of a great journey
+// Array of image file names
+const images = ['0_light.png', '1_light.png', '2_light.png', '3_light.png'];
+
+// Preload images
+const imageObjects = images.map(imageName => {
+    const image = new Image();
+    image.src = `assets/png/garage/${imageName}`;
+    return image;
+});
+
+// Function to switch the image
+function switchImage(direction) {
+    const imgElement = document.getElementById('img');
+    const currentImageIndex = images.indexOf(imgElement.src.split('/').pop());
+
+    let newIndex;
+
+    if (direction === 'left') {
+        newIndex = (currentImageIndex - 1 + images.length) % images.length;
+    } else if (direction === 'right') {
+        newIndex = (currentImageIndex + 1) % images.length;
+    }
+
+    imgElement.src = imageObjects[newIndex].src;
+}
+
+// Function to update container size
+function updateContainerSize() {
+    // Get the image element
+    var image = document.getElementById('img');
+
+    // Get the container element
+    var container = document.getElementById('container');
+
+    // Set the container size based on the image size
+    container.style.width = image.width + 'px';
+    container.style.height = image.height + 'px';
+}
+
+// Initial update on page load
+window.onload = updateContainerSize;
+
+// Update container size on window resize
+window.addEventListener('resize', updateContainerSize);

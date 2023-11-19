cf2311da5581830607fe7ebe30e487950339f1e9
Michellen Nguyen <michellenguyen236@gmail.com>
2023-11-18 22:02:09 -0500

    adding end game for typing game

--- webSite/src/assets/js/typingGame.js
+++ webSite/src/assets/js/typingGame.js
@@ -1,7 +1,22 @@
 
-const words = ["programming", "javascript", "developer", "keyboard", "challenge", "coding", "html", "css", "gpt", "challenge"];
-let currentWordIndex = 0;
-let targetWord = words[currentWordIndex];
+const words_array = [ 
+  "Happy", "Monster", "Sunshine", "Rainbow", "Friendship", "Adventure", "Magic", "Brave", "Laughter", "Teddy",
+  "Castle", "Butterfly", "Cupcake", "Dream", "Smiles", "Treasure", "Unicorn", "Superhero", "Puppy", "Popcorn","Joyful", "Snuggle",
+  "Moonlight", "Playful", "Ocean", "Hug", "Harmony", "Enchanting", "Giggle", "Exploration", "Cookie", "Starlight", "Cuddle", "Wonder",
+  "Blossom", "Serenade", "Jolly", "Pajamas", "Giggly", "Quest", "Radiant", "Lullaby", "Sweetheart", "Candy", "Whisper",
+  "Cherish", "Delightful", "Sunshine", "Gummybear", "Wondrous", "Pillow", "Happiness", "Dazzle", "Rainbow", "Harmony",
+  "Blissful", "Muffin", "Harmony", "Fluffy", "Hug",  "Mischief", "Cheerful", "Enchantment", "Playful", "Cozy","Harmony", "Lollipop", 
+  "Delight", "Cozy", "Cuddle", "Glitter", "Snuggle"
+];
+
+function getRandomWord() {
+  const randomIndex = Math.floor(Math.random() * words_array.length);
+  return words_array[randomIndex];
+}
+
+let targetWord = getRandomWord();
+let wordsCount = 0;
+
 
 // Display the initial word
 displayWord();
@@ -33,13 +48,26 @@ function displayWord() {
 
 }
 
-
 function displayWritten(char) {
     if (char === "") {
         document.getElementById('wordWritten').textContent = "";
     } else {
         document.getElementById('wordWritten').textContent = document.getElementById('wordWritten').textContent + char;
     }
+}
+
+function handleEndFight() {
+  document.getElementById('overlay').style.display = 'block';
+
+  let label = document.createElement('div');
+  label.id = 'endLabel';
+  label.innerText = 'You defeated the monster, well done!';
+  document.body.appendChild(label);
+
+  setTimeout(function () {
+    window.location.href = 'index.html';
+  }, 3000); // 5000 milliseconds = 5 seconds
+
 }
 
 function handleKeyPress(event) {
@@ -54,9 +82,15 @@ function handleKeyPress(event) {
 
     if (targetWord === "") {
       // Move to the next word when the current one is completed
+      wordsCount++;
+      console.log(wordsCount);
+
+      if (wordsCount === 5) {
+        handleEndFight();
+      }
+
       displayWritten("");
-      currentWordIndex = (currentWordIndex + 1) % words.length;
-      targetWord = words[currentWordIndex];
+      targetWord = getRandomWord();
       displayWord();
     }
   }
